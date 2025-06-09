7529bbbe55a336f27c65d49961802ccdc47f641a
Igor_Titov <mrfreiii@gmail.com>
2025-06-09 15:43:11 +0300

    renamed rate limits table

--- src/settings.ts
+++ src/settings.ts
@@ -13,6 +13,6 @@ export const SETTINGS = {
     USERS_REGISTRATION_INFO: 'public."UsersRegistrationInfo"',
     USERS_PASSWORD_RECOVERY_INFO: 'public."UsersPasswordRecoveryInfo"',
     SESSIONS: 'public."Sessions"',
-    RATE_LIMIT: 'public."RateLimit"',
+    RATE_LIMIT: 'public."RateLimits"',
   },
 };
